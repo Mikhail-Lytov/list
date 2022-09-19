65be6f93403685f2e85a95898d7d7ffea88d10ff
Mikhail-Lytov <Misha.Lytov@yandex.ru>
2022-09-19 16:20:54 +0300

    промежуток

--- src/myList.java
+++ src/myList.java
@@ -20,16 +20,33 @@ public class myList<E> {
         Node<E> next = first_element;
         next.setItem(element);
         first_element = new Node<E>(null,null,next);
-        next.setNext(first_element);
+        next.setPrev(first_element);
         number_elements++;
     }
 
     public void print_mylist(){
-        Node<E> perem = first_element;
-        for(int i =0; i < number_elements - 1; i++){
-            System.out.println(perem.getItem());
-            perem = perem.getNext();
+        Node<E> variable = first_element;
+        for(int i =0; i < number_elements; i++){
+            variable = variable.getNext();
+            E lox = variable.getItem();
+            System.out.println(variable.getItem());
 
+        }
+    }
+    public void copy_list(myList original, myList copy){ // мы взяли весь список
+        Node<E> first_element_copy = new Node<E>(null,null,null);
+        Node<E> last_element_copy = new Node<>(null,first_element_copy,null);
+        first_element_copy.setNext(last_element_copy);
+        int i = 0;
+        while (original.getItem() != null && i > 0){
+            E element_copy= original.getItem();
+            Node<E> prev_copy = original.getPrev();
+            Node<E> next_copy = original.getNext();
+            Node<E> pos_elment_copy = new Node<E>(element_copy,prev_copy,next_copy);
+            i++;
+            //original = original.getNext();
+            //System.out.println("i");
+            new Node<E>(null, pos_elment_copy, null);
         }
     }
     public int size_list(){
@@ -40,6 +57,40 @@ public class myList<E> {
         first_element.setItem(null);
         first_element.setPrev(null);
         number_elements--;
+    }
+    public void delete_end_elemetn_mylist(){
+        last_element = last_element.getPrev();
+        last_element.setItem(null);
+        last_element.setNext(null);
+        number_elements--;
+    }
+  /*  public void delete_by_value(E element){
+        Node<E> variable = first_element.getNext();
+        for (int i =0; i < number_elements ; i++){
+            //E arr = variable.getItem();
+            E variaable_name = variable.getItem();
+            if(variaable_name == element){
+                if(i == 0){
+
+                }else if(i > 0 && i < number_elements-1 ){
+
+                }else if(i == number_elements - 1){
+                    variable = variable.getPrev();
+                    System.out.println(variable.getItem());
+                    last_element = new Node<E>(null,variable,null);
+                    variable.setNext(last_element);
+                    //number_elements--;
+                break;
+                }
+            }
+            variable = variable.getNext();
+        }
+    }*/
+    public void clear_mylist(){
+        Node<E> variable = first_element;
+       last_element.setPrev(first_element);
+       first_element.setNext(last_element);
+       number_elements = 0;
     }
 
     private static class Node<E>{
@@ -70,6 +121,10 @@ public class myList<E> {
         public Node<E> getPrev(){
             return prev;
         }
+
+    }
+    public int getSize(){
+        return number_elements;
     }
 
 }
--- src/test_list.java
+++ src/test_list.java
@@ -1,11 +1,16 @@
 public class test_list {
     public static void main(String[] args){
     myList list = new myList();
-    list.set_begin_element_mylist("privet");
-    list.set_end_element_myList(1);
-    list.set_begin_element_mylist(1);
-    System.out.println(list.size_list());
+    list.set_begin_element_mylist(5);
+    list.set_end_element_myList("privet");
+    list.set_begin_element_mylist(4);
+    myList copy = new myList<>();
+    copy.copy_list(list,copy);
+    //System.out.println(list.size_list());
     //list.delete_begin_elemetn_mylist();
+    //list.delete_end_elemetn_mylist();
+        //list.delete_by_value("privet");
+    //list.clear_mylist();
     list.print_mylist();
     }
 }
