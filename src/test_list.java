f16fe46504ec62d71619db12d60c431f358febdd
Mikhail-Lytov <Misha.Lytov@yandex.ru>
2022-09-19 20:24:37 +0300

    V_1

--- src/test_list.java
+++ src/test_list.java
@@ -1,17 +1,32 @@
 public class test_list {
     public static void main(String[] args){
-    myList list = new myList();
-    list.set_begin_element_mylist(5);
-    list.set_end_element_myList("privet");
-    list.set_begin_element_mylist(4);
-    myList copy = new myList<>();
-    copy.copy_list(list,copy);
-    //System.out.println(list.size_list());
-    //list.delete_begin_elemetn_mylist();
-    //list.delete_end_elemetn_mylist();
-        //list.delete_by_value("privet");
-    //list.clear_mylist();
-        copy.delete_by_value(4);
-    copy.print_mylist();
+        myList list = new myList<>();//создание списка
+        list.set_begin_element_mylist("Lytov");//добавим элемент в начало
+        list.set_begin_element_mylist("Misha");//добавим элемент в начало
+        list.set_end_element_myList("ККСО-03-20"); // добавим элемент в конец
+        list.set_begin_element_mylist("13"); // добавим элемент в начало
+        list.print_mylist();//распечатаем список
+        myList copy = new myList<>();//создадим ещё один список
+        copy.copy_list(list,copy);//скопировали один список в другой
+        System.out.println("копия данных");
+        copy.print_mylist();
+        copy.delete_begin_elemetn_mylist();
+        System.out.println("удалили в начале списка: ");
+        copy.print_mylist();
+        copy.delete_end_elemetn_mylist();
+        System.out.println("удалили в конце списка: ");
+        copy.print_mylist();
+        list.delete_by_value("Misha"); // удаление по значению
+        System.out.println("что осталось после поэлементного удаление");
+        list.print_mylist();
+         int size = list.size_list();
+        System.out.println("сколько там элементов: " + size);
+        list.print_mylist();
+        System.out.println("удалим список");
+        list.clear_mylist();
+        size= list.getSize();
+        System.out.print("осталось данных: " + size);
+
+
     }
 }
